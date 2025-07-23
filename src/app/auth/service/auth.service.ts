e4d5645d4866651ab61f58977381747ba6edeb30
Taimoor Anees <shaykhtimur@gmail.com>
2025-07-23 22:40:13 +0500

    Login Frontend Done

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
- private apiUrl = environment.apiUrl;
-  constructor(private http: HttpClient) { }
+  private apiUrl = 'http://localhost:7000/api/auth';
+  private apiUrlSignup = environment.apiUrl;
 
-  register(payload:any): Observable<any>{
-    return this.http.post(`${this.apiUrl}/signup`, payload);
+  constructor(private http: HttpClient, private router: Router) {}
+
+  loginUser(credentials: { email: string; password: string }): Observable<any> {
+    console.log('Sending login request to:', `${this.apiUrl}/login`);
+    return this.http.post<any>(`${this.apiUrl}/login`, credentials);
+  }
+
+  storeToken(token: string): void {
+    localStorage.setItem('Auth Token', token);
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
+
+  register(payload: any): Observable<any> {
+    return this.http.post(`${this.apiUrlSignup}/signup`, payload);
   }
 }
